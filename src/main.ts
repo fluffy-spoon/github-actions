a07152fafb17a372bf0bf4697806d8d907544729
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-11-01 22:46:04 +0100

    qwd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import glob from 'glob';
 console.log('foobar');
 
 glob("**/*.sln", {}, (err, files) => {
+    console.log('files?', err, files);
+
     for(let file of files) {
         console.log(file);
     }
 });
+
+console.log('donebuz');
