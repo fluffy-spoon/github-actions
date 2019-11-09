bf9724d531ad99ac30a915edc5172929ed6e6912
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-11-09 23:28:28 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -5458,7 +5458,7 @@ async function gitCommand(args) {
 }
 async function gitCheckout() {
     let github = await environment_1.getGitHubContext();
-    await gitCommand(['clone', github.repository.git_url]);
+    await gitCommand(['clone', github.repository.clone_url]);
     // await gitCommand(['init', github.environment.WORKSPACE]);
     // await gitCommand(['remote', 'add', 'origin', github.repository.html_url]);
     // await gitCommand(['config', 'gc.auto', '0']);
--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function gitCommand(args: string[]) {
 async function gitCheckout() {
     let github = await getGitHubContext();
 
-    await gitCommand(['clone', github.repository.git_url]);
+    await gitCommand(['clone', github.repository.clone_url]);
 
     // await gitCommand(['init', github.environment.WORKSPACE]);
     // await gitCommand(['remote', 'add', 'origin', github.repository.html_url]);
